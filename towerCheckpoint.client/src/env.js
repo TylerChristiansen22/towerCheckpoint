e4819ec272a65e46c5a8b8adc45df57e290135bd
TylerChristiansen22 <tyguy.tc@gmail.com>
2023-09-19 17:02:34 -0600

    Init

---
+++ towerCheckpoint.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'dev-r2os7r1rjtqgn1eq.us.auth0.com'
+export const clientId = 'csWIVVBxuAbwznFOw13vl3cyl27sz7qf'
+export const audience = "https://Tyler'sAPI.com"
